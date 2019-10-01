432fa134029d4a0835ca0bd89a1f3735df08dd54
hunaba <abadeer.hunson@yandex.ru>
2019-10-01 20:31:17 +0200

    liste mail

---
+++ exo_18.rb
@@ -0,0 +1,11 @@
+counter = 1
+mailing_list = ["jeandupont#{counter}@email.fr"]
+
+counter = 2
+while (counter <= 50)
+	mailing_list << "jeandupont#{counter}@email.fr"
+	counter += 1
+end 
+
+
+#pourle19 rajoutr des 2 à ch f // Écris un programme exo_18.rb qui va créer une liste de 50 faux emails et les stocker dans une array. 
